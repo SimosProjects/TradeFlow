3ef44fe7440b00dd4db3ddd8bddd75b8a6ece4b2
Christopher Simonson <simonson.dev@gmail.com>
2026-05-13 22:38:52 +0700

    created initial testing, xUnit, AlertClassifier tests

---
+++ TradeFlow.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using TradeFlow.AlertPoC.Models;
+global using TradeFlow.AlertPoC.Services;
+global using TradeFlow.AlertPoC.RiskEngine;
