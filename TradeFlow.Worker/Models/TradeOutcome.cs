919f7b1bd880e9759c62060456635cb183e51f95
Christopher Simonson <simonson.dev@gmail.com>
2026-05-15 16:28:55 +0700

    added broker models for trades

---
+++ TradeFlow.Worker/Models/TradeOutcome.cs
@@ -0,0 +1,11 @@
+namespace TradeFlow.Worker.Models;
+
+public enum TradeOutcome
+{
+    Open,         // still running
+    TargetHit,    // profit target reached
+    StoppedOut,   // stop loss hit
+    XtradesExit,  // closed by Xtrades stc signal
+    Averaged,     // averaged into position
+    Cancelled     // order cancelled
+}
